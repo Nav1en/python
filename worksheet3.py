fa4dbe980959db3cdefb215d3eacc6a9172da77d
Nav1en <147179515+Nav1en@users.noreply.github.com>
2023-10-06 16:42:53 +0100

    Create worksheet3.py

---
+++ worksheet3.py
@@ -0,0 +1,19 @@
+print("Task 1")
+print("------------------------------------------------------------------------")
+age = input("What is your age? ")
+age = int(age)
+if age < 26 or age > 60:
+ print("Reduced")
+else:
+ print("full price")
+
+print("------------------------------------------------------------------------")
+print("Task 2")
+
+a = input("Enter your first word ")
+b = input("Enter your second word ")
+
+if a == b:
+ print(a)
+else:
+ print(a + b)
